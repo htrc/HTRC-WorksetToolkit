b617db83c46cb7e3d258915c25c4ba16780f82f2
Jaimie Murdock <jaimie.murdock@gmail.com>
2017-03-10 14:52:08 -0500

    adding unit test for bool_prompt

---
+++ tests/test_htrc_lib_cli.py
@@ -0,0 +1,34 @@
+from __future__ import print_function
+from future import standard_library
+standard_library.install_aliases()
+
+import sys
+if sys.version_info.major == 2:
+    from mock import Mock, patch, PropertyMock
+elif sys.version_info.major == 3:
+    from unittest.mock import Mock, patch, PropertyMock
+
+import unittest2 as unittest
+
+from htrc.lib.cli import *
+
+class TestVolumes(unittest.TestCase):
+    @patch('htrc.lib.cli.input')
+    def test_credential_prompt(self, input_mock):
+        # test True
+        input_mock.return_value = 'y'
+        return_value = bool_prompt("Enter yes")
+        self.assertEqual(return_value, True)
+        
+        input_mock.return_value = 'n'
+        return_value = bool_prompt("Enter no")
+        self.assertEqual(return_value, False)
+
+        input_mock.return_value = ''
+        return_value = bool_prompt("Enter nothing for false", default=False)
+        self.assertEqual(return_value, False)
+        
+        return_value = bool_prompt("Enter nothing for true", default=True)
+        self.assertEqual(return_value, True)
+
+
