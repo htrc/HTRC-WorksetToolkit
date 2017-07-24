10218824a69633a7dc146cb16886b4b2f616921b
Jaimie Murdock <jaimie.murdock@gmail.com>
2017-07-23 20:31:22 -0400

    adding config file support for urls

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 import platform
 import sys
 
-__version__ = '0.1.23'
+__version__ = '0.1.24'
 
 install_requires = [ 'PyLD', 'future', 'prov', 'unicodecsv', 'progressbar2',
     'requests', 'wget', 'sphinx-argparse', 'sphinxcontrib-fulltoc']
