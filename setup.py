df56fd31d3e7ffc7d5df026a287bf71e71d4ab86
Jaimie Murdock <jaimie.murdock@gmail.com>
2018-02-08 12:20:56 -0500

    version bump for mallet, topicexplorer, and jwt fixes

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 import platform
 import sys
 
-__version__ = '0.1.33'
+__version__ = '0.1.36'
 
 install_requires = [ 'PyLD', 'future', 'prov', 'unicodecsv', 'progressbar2',
     'requests', 'wget', 'argparse==1.1', 'topicexplorer>=1.0b193']
