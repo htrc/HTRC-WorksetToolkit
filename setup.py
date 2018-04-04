857ff90a1f0dacd9b76769c1f959ec622eeb7109
Samitha Liyanage <samitha.liyanage@gmail.com>
2018-04-04 15:54:54 -0400

    Correction on topicexplorer arguments

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import atexit
 import tarfile
 import wget
 
-__version__ = '0.1.44b0'
+__version__ = '0.1.44b1'
 
 install_requires = ['PyLD', 'future', 'prov', 'unicodecsv', 'progressbar2',
                     'requests', 'wget', 'argparse==1.1', 'topicexplorer>=1.0b194']
